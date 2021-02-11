a83cef9dd9c02904c6773cce55430fecbf577b06
dimitris <jim_pap@live.com>
2021-02-11 13:02:13 +0200

    a

--- users/urls.py
+++ users/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(next_page='weather:home'), name='logout'),
     path('register/', views.register_view, name='register'),
+    ###alalala
 ]
